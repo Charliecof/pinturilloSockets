5a3605df8d92b5dc89b87ef0f5676861a437f427
Charliecof <a01328800@itesm.mx>
2022-05-21 22:51:22 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const server = http.createServer(app);
+const io = new Server(server, {
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+	},
+});
+
+app.use(cors);
+
+io.on('connection', (socket) => {
+    socket.join('test')
+	console.log('user');
+	console.log('Clients number', io.engine.clientsCount);
+	socket.on('drawingSend', (data) => {
+		io.emit('otherData', data);
+	});
+});
+
+app.get('/', (req, res, next) => {
+	res.send('Hola');
+});
+
+server.listen(4000, () => console.log('Server running on port 4000'));
